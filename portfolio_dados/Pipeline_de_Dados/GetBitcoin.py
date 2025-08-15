b3e49c7b857fa4649e13d3c8623d7905e548beee
EriickHenriique <eriickhenriique.s.silva@gmail.com>
2025-08-15 19:24:02 -0300

    Removendo submódulo e adicionando pasta normal

---
+++ portfolio_dados/Pipeline_de_Dados/GetBitcoin.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime
+import pandas as pd
+
+def get_bitcoin_df():
+
+    # URL da Coinbase para obter o preço atualizado do Bitcoin
+    url = "https://api.coinbase.com/v2/prices/spot"
+
+    #Requisição na API e Definição da variável
+    response = requests.get(url)
+    data = response.json()
+
+    #Separando as informações do JSON
+    price = float(data['data']['amount'])
+    asset = data['data']['base']
+    currency = data['data']['currency']
+    time = datetime.now().strftime('%H:%M:%S')
+
+    # Criação do DataFrame
+    df = pd.DataFrame([{
+        'horário': time,
+        'ativo': asset,
+        'preço': price,
+        'moeda': currency
+    }])
+
+    return df
+
+
