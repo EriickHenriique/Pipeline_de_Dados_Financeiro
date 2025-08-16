9435612a207223a083c94125f678a8c4749b0d9a
EriickHenriique <eriickhenriique.s.silva@gmail.com>
2025-08-15 23:59:09 -0300

    feat: código funcional

--- Pipeline_de_Dados/GetCommodities.py
+++ Pipeline_de_Dados/GetCommodities.py
@@ -7,11 +7,11 @@ def get_commodities_df():
     dfs = []
     for syn in symbols:
         ultimo_df = yf.Ticker(syn).history(period='1d', interval='1m')[['Close']].tail(1)
-        ultimo_df = ultimo_df.rename(columns={'Close':'Preço'})
-        ultimo_df['Ativo'] = syn
-        ultimo_df['Moeda'] = 'USD'
-        ultimo_df['Horário'] = datetime.now().strftime('%H:%M:%S')
-        ultimo_df = ultimo_df[['Horário','Ativo','Preço','Moeda']]
+        ultimo_df = ultimo_df.rename(columns={'Close':'preco'})
+        ultimo_df['ativo'] = syn
+        ultimo_df['moeda'] = 'USD'
+        ultimo_df['hora'] = datetime.now().strftime('%H:%M:%S')
+        ultimo_df = ultimo_df[['hora','ativo','preco','moeda']]
         dfs.append(ultimo_df)
     return pd.concat(dfs, ignore_index=True)
 
