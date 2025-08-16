b8e77bc0dcce08c4394e9d8724503d3920c590b6
EriickHenriique <eriickhenriique.s.silva@gmail.com>
2025-08-16 12:14:55 -0300

    atualização de comentários

--- Pipeline_de_Dados/GetCommodities.py
+++ Pipeline_de_Dados/GetCommodities.py
@@ -2,9 +2,12 @@ import yfinance as yf
 from datetime import datetime
 import pandas as pd
 
+#Função para obter informações das commodities
 def get_commodities_df():
+    #Lista de Ativos que iremos obter
     symbols = ['GC=F','CL=F','SI=F']
     dfs = []
+    #Loop para obter informações de cada ativo na lista symbols
     for syn in symbols:
         ultimo_df = yf.Ticker(syn).history(period='1d', interval='1m')[['Close']].tail(1)
         ultimo_df = ultimo_df.rename(columns={'Close':'preco'})
